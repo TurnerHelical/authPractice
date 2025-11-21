d961f387dd7f412033d16e5ca10fbc0784be7844
TurnerHelical <hunterde91@gmail.com>
2025-11-21 16:06:11 -0500

    changed the schema js file to properly create the tables the app needs

--- db/schema.js
+++ db/schema.js
@@ -1,13 +1,15 @@
+const {Client} = require('pg')
+
 const SQL = `
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         username TEXT NOT NULL UNIQUE,
-        password_hash NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
         first_name TEXT,
         last_name TEXT,
         email VARCHAR(254) NOT NULL UNIQUE,
         created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-        )
+        );
     
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -19,4 +21,16 @@ const SQL = `
             REFERENCES users(id)
             ON DELETE CASCADE
         );
-`
+`;
+
+async function main() {
+    const client = new Client({
+        connectionString: 'postgresql://hunter:Ellody2019@localhost:5432/members_message'
+    })
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log('done');
+};
+
+main();
