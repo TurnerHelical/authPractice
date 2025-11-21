52fc347b413d10e74a4d6169bbb4808091ad72af
TurnerHelical <hunterde91@gmail.com>
2025-11-21 11:54:58 -0500

    created user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,4 +4,12 @@ const router = Router();
 
 const controller = require('../controllers/userController');
 
+router.get('/logIn', controller.logInPageGet);
+router.post('/logIn', controller.logInPagePost);
+
+router.get('/signUp', controller.signUpPageGet);
+router.post('/signUp', controller.signUpPagePost);
+
+router.get('/dash/:id', controller.dashPageGet);
+
 module.exports = router;
